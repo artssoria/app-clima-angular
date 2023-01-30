6a16ed203fead42b92f1151dfc52bff69d8ddea0
artssoria <53129227+artssoria@users.noreply.github.com>
2023-01-29 21:44:30 -0300

    subiendo repo

---
+++ src/app/services/clima.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClimaService {
+url = 'https://api.openweathermap.org/data/2.5/weather?&appid=';
+  key = 'b187123edb7b99d2dbcfab80ae642a10';
+
+  constructor(private http: HttpClient) { }
+
+  getClima(ciudad: string): Observable<any>{
+    const URL = this.url + this.key + '&q=' + ciudad;
+     return this.http.get(URL);
+
+  }
+}
